584bade3c960d98c10b5f0406e985d30ea5e8974
unknown <jean.bahia@basis.com.br>
2024-08-09 20:32:31 -0300

    Incluindo código referente ao desafio do contador.

---
+++ src/main/java/br/com/jeanhbahia/contador/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package br.com.jeanhbahia.contador;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
